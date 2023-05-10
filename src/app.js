139ca4434378e4917e9eda7fb632278c5286267d
alexandrekws <alexandrekws@uol.com.br>
2023-05-10 10:36:42 +0900

    change aws ambient env

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import { Sequelize } from "sequelize";
 
 import "./database";
 
+require("dotenv").config();
+
 const port = process.env.PORT || 3001;
 const sequelize = new Sequelize('postgres://admin:admin@localhost:5432/mydb', {
   dialectModule: require('pg')
--- src/config/multer.js
+++ src/config/multer.js
@@ -1,12 +1,24 @@
 import multer from 'multer'
 import { v4 } from 'uuid'
 import { extname, resolve } from 'path'
+import multerS3 from 'multer-s3'
+import aws from 'aws-sdk'
 
-export default {
-    storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', 'uploads'),
-        filename: (request, file, callback) => {
+export default {  
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'uploadcodeburger',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, callback) => {
             return callback(null, v4() + extname(file.originalname))
         },
-    }),  
+    }),
 }
+
+// storage: multer.diskStorage({
+//     destination: resolve(__dirname, '..', '..', 'uploads'),
+//     filename: (request, file, callback) => {
+//         return callback(null, v4() + extname(file.originalname))
+//     },
+// }), 
